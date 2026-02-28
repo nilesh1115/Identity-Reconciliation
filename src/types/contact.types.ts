900d2ec3000599c24a88029b5c32a6026860324b
Nilesh <nileshnikam0501@gmail.com>
2026-02-28 23:13:42 +0530

    add config and contact types

---
+++ src/types/contact.types.ts
@@ -0,0 +1,24 @@
+export interface IdentifyRequest {
+  email?: string;
+  phoneNumber?: string;
+}
+
+export interface IdentifyResponse {
+  contact: {
+    primaryContactId: number;
+    emails: string[];
+    phoneNumbers: string[];
+    secondaryContactIds: number[];
+  };
+}
+
+export interface ContactData {
+  id: number;
+  phoneNumber: string | null;
+  email: string | null;
+  linkedId: number | null;
+  linkPrecedence: 'primary' | 'secondary';
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date | null;
+}
